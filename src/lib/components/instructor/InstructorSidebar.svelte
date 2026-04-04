fd939c0d4fb34445d5688b3d86df86116ef44940
unknown <dejesuskharl32@gmail.com>
2026-04-04 22:42:25 +0800

    Modified: Remove icon nav in sidebar (Instructor)

--- src/lib/components/instructor/InstructorSidebar.svelte
+++ src/lib/components/instructor/InstructorSidebar.svelte
@@ -42,12 +42,6 @@
 			name: 'Operations',
 			href: '/instructor/operations',
 			icon: 'M15 12a3 3 0 11-6 0 3 3 0 016 0z M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z'
-		},
-		{
-			name: 'Notifications',
-			href: '/instructor/notifications',
-			icon: 'M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9',
-			badge: 0 // Will be dynamic - unread notifications
 		}
 	];
 	
