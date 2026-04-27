55bdef856c20584ac7879f7eeb89ecc8630d0220
unknown <dejesuskharl32@gmail.com>
2026-04-27 09:24:31 +0800

    Removed: Shortcut key nav and file (Superadmin)

--- src/lib/components/superadmin/SuperadminSidebar.svelte
+++ src/lib/components/superadmin/SuperadminSidebar.svelte
@@ -37,7 +37,6 @@
 		analytics: false,
 		auditLogs: false,
 		inventory: false,
-		shortcutKeys: false,
 		system: false,
 		security: false
 	});
@@ -112,11 +111,6 @@
 		{
 			title: 'System Administration',
 			items: [
-				{
-					name: 'Shortcut Keys',
-					href: '/superadmin/shortcut-keys',
-					icon: 'M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z'
-				},
 				{
 					name: 'Settings',
 					href: '/superadmin/settings',
